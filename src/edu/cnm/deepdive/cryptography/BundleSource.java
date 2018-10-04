28a9eddb0e601a26f85dae10e4376f24b9387176
Nick Bennett <nick@nickbenn.com>
2018-10-04 12:00:59 -0600

    Initial commit with core classes.

---
+++ src/edu/cnm/deepdive/cryptography/BundleSource.java
@@ -0,0 +1,30 @@
+package edu.cnm.deepdive.cryptography;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class BundleSource implements WordSource {
+
+  private ArrayList<String> words;
+
+  public BundleSource(String basename) {
+    ResourceBundle bundle = ResourceBundle.getBundle(basename);
+    words = new ArrayList<>(
+        bundle.keySet().stream()
+            .map((k) -> bundle.getString(k))
+            .collect(Collectors.toList())
+    );
+  }
+
+  @Override
+  public String get(int i) {
+    return words.get(i);
+  }
+
+  @Override
+  public int size() {
+    return words.size();
+  }
+
+}
